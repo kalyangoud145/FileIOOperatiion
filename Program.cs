ba99f09249ba1cd2f4aac9ffed6bc3a2b9653cee
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-11 22:08:10 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+
+namespace FileIOOperatiion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to problem");
+        }
+
+    }
+}
